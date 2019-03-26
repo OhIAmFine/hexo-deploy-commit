890d907b535df88d89ca8277486773281e6d8b95
OhIAmFine <845782625@qq.com>
2019-03-26 15:52:02 +0800

    optimize code

--- hexo-deploy-commit.sh
+++ hexo-deploy-commit.sh
@@ -4,10 +4,8 @@ sed -e 's/=/:/g;s/"/ /g' file.sh> _config1.yml
 
 .  ./file.sh
 
-# newMessage=$message
-
 commitMessage=$1
 
-sed -i 'back' "s/$message/$commitMessage/g" _config1.yml
+sed -i '' "s/$message/$commitMessage/g" _config1.yml
 
 echo "commit message is : ${message}, ${commitMessage}"
